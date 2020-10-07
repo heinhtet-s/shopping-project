fa21b15841c3175017c05c40d647db072c53c599
LAPTOP-FUU6KJCJ\ASUS <heinh9540@gmail.com>
2020-10-07 14:26:30 +0630

    update update

--- sale_order.php
+++ sale_order.php
@@ -2,9 +2,6 @@
 require "config/common.php";
 require "config/config.php";
 session_start();
-if(empty($_SESSION['user_id'])&& empty($_SESSION['logged_in'])){
-	header('Location: login.php');
-  }
 if(!empty($_SESSION['cart'])){
 $total=0;
 foreach ($_SESSION['cart'] as $key => $qty) {
